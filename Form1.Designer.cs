7682efd56566563bc5def18a607e929c6f8d8636
hilal <hilalcerit12@gmail.com>
2022-05-06 00:16:58 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Location = new System.Drawing.Point(-2, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 65);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get All Data";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.getAllDataButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-2, 323);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(871, 223);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Location = new System.Drawing.Point(-2, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 65);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(500, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Id :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(615, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(615, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(500, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Product Name :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(615, 96);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(125, 27);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(500, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Price :";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(615, 138);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(125, 27);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(500, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Url :";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(615, 179);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(125, 27);
+            this.textBox5.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(500, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Description :";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button3.Location = new System.Drawing.Point(-2, 155);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(228, 65);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Delete Data";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.deleteDataButton_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button4.Location = new System.Drawing.Point(-2, 226);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(228, 65);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Update";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.updateDataButton_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button5.Location = new System.Drawing.Point(254, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(186, 65);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Search Data ";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.searchDataButton_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button6.Location = new System.Drawing.Point(254, 84);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(186, 86);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "Quantity of Products";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.showQuantityofProductsButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(500, 233);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(158, 20);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Total Product Number:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(686, 233);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 20);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 546);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LoginRegistrationInMVC;Integrated Security=True;");
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void getAllDataButton_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("Select * from Products", connection);
+            DataSet ds = new DataSet();
+            sqlDataAdapter.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+
+
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand saveData = new SqlCommand("insert into Products (Id,ProductName,Price,Url,Description) values (@p1,@p2,@p3,@p4,@p5)",connection);
+            saveData.Parameters.AddWithValue("@p1", textBox1.Text);
+            saveData.Parameters.AddWithValue("@p2", textBox2.Text);
+            saveData.Parameters.AddWithValue("@p3", textBox3.Text);
+            saveData.Parameters.AddWithValue("@p4", textBox4.Text);
+            saveData.Parameters.AddWithValue("@p5", textBox5.Text);
+
+            saveData.ExecuteNonQuery();
+            connection.Close();
+
+
+
+        }
+
+        private void deleteDataButton_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand deleteData = new SqlCommand("delete from Products where Id= @p1", connection);
+            deleteData.Parameters.AddWithValue("@p1", textBox1.Text);
+            deleteData.Parameters.AddWithValue("@p2", textBox2.Text);
+            deleteData.Parameters.AddWithValue("@p3", textBox3.Text);
+            deleteData.Parameters.AddWithValue("@p4", textBox4.Text);
+            deleteData.Parameters.AddWithValue("@p5", textBox5.Text);
+
+            deleteData.ExecuteNonQuery();
+            connection.Close();
+        }
+
+   
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int choosen = dataGridView1.SelectedCells[0].RowIndex;
+
+            string ıd = dataGridView1.Rows[choosen].Cells[0].Value.ToString();
+            string productName = dataGridView1.Rows[choosen].Cells[1].Value.ToString();
+            string price = dataGridView1.Rows[choosen].Cells[2].Value.ToString();
+            string url = dataGridView1.Rows[choosen].Cells[3].Value.ToString();
+            string description = dataGridView1.Rows[choosen].Cells[4].Value.ToString();
+
+            textBox1.Text = ıd;
+            textBox2.Text = productName;
+            textBox3.Text = price;
+            textBox4.Text = url;
+            textBox5.Text = description;
+      
+        }
+
+        private void updateDataButton_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand updateData = new SqlCommand("update Products set Id=@p1,ProductName=@p2,Price=@p3,Url=@p4,Description=@p5 where Id=@p1 ",connection);
+            updateData.Parameters.AddWithValue("@p1", textBox1.Text);
+            updateData.Parameters.AddWithValue("@p2", textBox2.Text);
+            updateData.Parameters.AddWithValue("@p3", textBox3.Text);
+            updateData.Parameters.AddWithValue("@p4", textBox4.Text);
+            updateData.Parameters.AddWithValue("@p5", textBox5.Text);
+
+            updateData.ExecuteNonQuery();
+            connection.Close();
+
+
+
+
+        }
+
+        private void searchDataButton_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlDataAdapter searchData = new SqlDataAdapter("select * from Products  where Id='"+textBox1.Text +"'", connection);
+            DataSet dataSet = new DataSet();
+            searchData.Fill(dataSet);
+            dataGridView1.DataSource = dataSet.Tables[0];
+
+        
+            connection.Close();
+
+        }
+
+        private void showQuantityofProductsButton_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select count(ProductName) from Products", connection);
+            SqlDataReader read = command.ExecuteReader();
+            while(read.Read())
+            {
+                label7.Text = read[0].ToString();
+
+            }
+            connection.Close();
+        }
+    }
+}
